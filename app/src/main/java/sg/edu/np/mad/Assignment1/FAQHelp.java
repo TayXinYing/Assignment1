48d8202d3468ddb0a6791cdae2f50a3022af8bc9
ChorYi <104066208+ChorYi@users.noreply.github.com>
2022-06-20 06:04:13 +0800

    Implementing FAQ page (2)

--- app/src/main/java/sg/edu/np/mad/Assignment1/FAQHelp.java
+++ app/src/main/java/sg/edu/np/mad/Assignment1/FAQHelp.java
@@ -3,12 +3,19 @@ package sg.edu.np.mad.Assignment1;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FAQHelp extends AppCompatActivity {
+    
+    RecyclerView recyclerView;
+    List<FAQ> faqList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +31,29 @@ public class FAQHelp extends AppCompatActivity {
 
         // showing the back button in action bar
         actionBar.setDisplayHomeAsUpEnabled(true);
+        
+        // Display FAQ
+        recyclerView = findViewById(R.id.faq_recyclerView);
+
+        initData();
+        setRecyclerView();
+    }
+    
+    private void setRecyclerView(){
+        FAQAdapter faqAdapter = new FAQAdapter(faqList);
+        recyclerView.setAdapter(faqAdapter);
+        //recyclerView.setHasFixedSize(true);
     }
 
+    private void initData(){
+        faqList = new ArrayList<>();
+
+        faqList.add(new FAQ("What is this application about?", "Recyclops is a platform that allows users to list secondhand items and learn upcycling. Unlike Carousell, which allows people to sell new or old items for cash, our application only allows users to list secondhand items in order to raise awareness on the importance of buying second-hand and upcycling. Users can utilize this platform to buy supplies for their own projects and can also contribute their upcycling suggestions. Finally, through uploaded tutorials, users will be able to learn to upcycle in unique ways."));
+        faqList.add(new FAQ("What is the purpose of this application?", "Our application aims to promote upcycling through reusing and second-hand purchases in order to create a more sustainable world."));
+        faqList.add(new FAQ("Who is suited to use this application?", "Everyone! This application is developed to educate people of all ages on how to safeguard the environment by upcycling items and the importance of natural resource conservation."));
+        faqList.add(new FAQ("How can I contribute my upcycling ideas?", "There is an upload feature where user would be able to upload their upcycling tutorials to share it with other users."));
+    }
+    
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
